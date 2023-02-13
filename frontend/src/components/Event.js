f0955e95eae773575cee9b9237c42646c0673985
soultan.ali.h <soultan.ali.h@gmail.com>
2023-02-14 04:17:38 +0700

    set events

--- frontend/src/components/Event.js
+++ frontend/src/components/Event.js
@@ -1,28 +1,28 @@
 export const myevents = [
   {
     id: 0,
-    title: "training",
+    title: "Pelatihan-Uji Online SIMAK dan KPO 2022",
     start: new Date(2023, 1, 8, 9, 0, 0),
     end: new Date(2023, 1, 9, 13, 0, 0),
     resourceId: 1,
   },
   {
     id: 1,
-    title: "late lunch",
+    title: "Drill & Blast - WEEKLY REFRESH COMPETENCY",
     start: new Date(2023, 1, 6, 14, 0, 0),
     end: new Date(2023, 1, 8, 16, 30, 0),
     resourceId: 2,
   },
   {
     id: 2,
-    title: "fight",
+    title: "DO & DON'T EKSPLORASI",
     start: new Date(2023, 1, 8, 8, 30, 0),
     end: new Date(2023, 1, 8, 12, 30, 0),
     resourceId: 3,
   },
   {
     id: 3,
-    title: "party",
+    title: "Pendidikan dan Pelatihan Pengelola Peledakan (KPP Pertama)",
     start: new Date(2023, 1, 8, 7, 0, 0),
     end: new Date(2023, 1, 8, 10, 30, 0),
     resourceId: 4,
@@ -30,8 +30,8 @@ export const myevents = [
 ];
 
 export const myresources = [
-  { id: 1, title: "spiderman" },
-  { id: 2, title: "batman" },
-  { id: 3, title: "aquaman" },
-  { id: 4, title: "microman" },
+  { id: 1, title: "1" },
+  { id: 2, title: "2" },
+  { id: 3, title: "3" },
+  { id: 4, title: "4" },
 ];
